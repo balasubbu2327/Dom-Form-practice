f2496492a35b0586da2c53bba6c2e7e8482e697e
Bala Subramanian <98680016+balasubbu2327@users.noreply.github.com>
2022-03-30 21:13:57 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,55 @@
+var div=document.createElement("div");
+div.classname="form";
+var fname=label("label","for","fname","FirstName");
+var fname1=input("input","type","text","id","fname","placeholder","firstname");
+var br1= document.createElement("br")
+var lname=label("label","for","lname","LastName");
+var lname1=input("input","type","text","id","lname","placeholder","lastname");
+var br2= document.createElement("br")
+var email=label("label","for","email","E-Mail");
+var email1=input("input","type","email","id","email","placeholder","abc@gmail.com");
+var br3= document.createElement("br")
+var pass=label("label","for","pwd","PassWord");
+var pass1=input("input","type","password","id","pwd","placeholder","passcode");
+var br4= document.createElement("br")
+div.append(fname,fname1,br1,lname,lname1,br2,email,email1,br3,pass,pass1,br4);
+document.body.append(div);
+function label (elementname,attribute,attrvalue,value){
+    var res = document.createElement(elementname);
+    res.setAttribute(attribute,attrvalue);
+    res.innerHTML=value;
+    return res;
+}
+function input (elementname,attribute,attrvalue,attr1,attr2,attr3,attr4){
+    var res1= document.createElement(elementname);
+    res1.setAttribute(attribute,attrvalue);
+    res1.setAttribute(attr1,attr2);
+    res1.setAttribute(attr3,attr4);
+    return res1;
+}
+var button=input("button","type","button","class","btn btn-primary","id","btn")
+button.innerHTML="click me";
+button.addEventListener("click",foo);
+document.body.append(button);
+function foo(){
+    var res1=document.getElementById("fname").value;
+    var res2=document.getElementById("lname").value;
+    var res3=document.getElementById("email").value;
+    var res4=document.getElementById("pwd").value;
+    console.log(`fname:${res1} lname:${res2} Email:${res3} pwd:${res4}`);
+}
+
+
+
+
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------//
+
+
+
+
+
+
 var div=document.createElement("div")
 div.classname="Form"
 var label1=document.createElement("label");
